b72eac055cc45144b571f57ea8421ae17ec89789
mohamedkhaled12 <mohamedkhaledelb.com>
2025-05-24 11:34:46 +0200

    echo impl

--- app/main.py
+++ app/main.py
@@ -10,12 +10,16 @@ def main():
         try:
             sys.stdout.write("$ ")
             command = input()
+            echo_part, rest_to_be_echoed = command.split(' ', 1)
+
+            if echo_part == 'echo':
+                print(f"{rest_to_be_echoed}")
         except EOFError:
             print()
             break
         if command.strip() == "exit 0":
             sys.exit(0)
-        print(f"{command}: command not found")
+        
 
 
 if __name__ == "__main__":
