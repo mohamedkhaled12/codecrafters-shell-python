d6951b98458d88db33c668a63359b8b34e059559
mohamedkhaled12 <mohamedkhaledelb@gmail.com>
2025-05-25 12:04:22 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,21 +1,44 @@
 import sys
+import os
+
+def type_cmd(cmd, *_):
+    if cmd in BUILTINS:
+        print(f"{cmd} is a shell builtin")
+    else:
+        for directory in os.getenv("PATH", "").split(":"):
+            full_path = os.path.join(directory, cmd)
+            if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                print(f"{cmd} is {full_path}")
+                return
+        print(f"{cmd}: not found")
+                
+            
+        
+    
+
 BUILTINS = {
     "exit": lambda code=0, *_: sys.exit(int(code)),
-    "echo": lambda *args: print(" ".join(args)),
-    "type": lambda cmd, *_: print(f"{cmd} is a shell builtin")
-    if cmd in BUILTINS
-    else print(f"{cmd}: not found"),
+    "echo": lambda *args : print(" ".join(args)),
+    "type": type_cmd,
 }
+
+
 def main():
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        usr_input = input().split()
-        cmd = usr_input[0]
-        args = usr_input[1:]
+        
+        user_input = input().split()
+        cmd = user_input[0]
+        args = user_input[1:]
+        
         if cmd in BUILTINS:
             BUILTINS[cmd](*args)
         else:
-            print(f"{cmd}: command not found")
+            print(f"{cmd}: command not found")    
+        
+
+
 if __name__ == "__main__":
     main()
+        
